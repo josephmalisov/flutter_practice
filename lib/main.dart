0e8e76d547b45a981e8fa7f505230173e45f437a
Joseph Malisov <j.a.malisov@gmail.com>
2020-05-20 18:21:32 -0400

    added second screen with input form

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,48 @@
 import 'package:flutter/material.dart';
-import 'shared.dart';
 
 part 'new_message.dart';
+
+
+class MyScaffold extends StatefulWidget {
+  _MyScaffoldState createState() => _MyScaffoldState();
+}
+
+class _MyScaffoldState extends State<MyScaffold> {
+  bool pressed = false;
+
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece of paper on which the UI appears.
+    return Scaffold(
+      // Column is a vertical, linear layout.
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child to fill the available space.
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: Text(""),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => NewMessage()),
+            );
+          },
+          child: Icon(Icons.add)),
+    );
+  }
+}
+
 
 /**
  *  main 
---
+++ lib/new_message.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+part of 'main.dart';
+
+class NewMessage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MyScaffold();
+  }
+}
+
+class MyForm extends Form {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Name *',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'Message *',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter a message';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // Process data.
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
