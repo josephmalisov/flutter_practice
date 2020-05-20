e7b6bef8163bfbf6419f0d84e60005af782e6a86
Joseph Malisov <j.a.malisov@gmail.com>
2020-05-20 18:43:07 -0400

    add message page displays correctly

--- lib/new_message.dart
+++ lib/new_message.dart
@@ -2,57 +2,89 @@ part of 'main.dart';
 
 class NewMessage extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MyScaffold();
+    return MyScaffold2();
+  }
+}
+
+class MyScaffold2 extends StatefulWidget {
+  _MyScaffoldState2 createState() => _MyScaffoldState2();
+}
+
+class _MyScaffoldState2 extends State<MyScaffold2> {
+  bool pressed = false;
+
+  Widget build(BuildContext context) {
+    final _formKey = GlobalKey<FormState>();
+
+    // Material is a conceptual piece of paper on which the UI appears.
+    return Scaffold(
+      // Column is a vertical, linear layout.
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: null, // null disables the button
+          ),
+          // Expanded expands its child to fill the available space.
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Name *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Message *',
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter a message';
+                }
+                return null;
+              },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate will return true if the form is valid, or false if
+                  // the form is invalid.
+                  if (_formKey.currentState.validate()) {
+                    // Process data.
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
 class MyForm extends Form {
-  final _formKey = GlobalKey<FormState>();
-
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Name *',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Message *',
-            ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter a message';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState.validate()) {
-                  // Process data.
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ),
-        ],
-      ),
-    );
+    return null;
   }
 }
